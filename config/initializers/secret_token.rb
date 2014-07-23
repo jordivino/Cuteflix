8ead4dd99add3a2e166ef3f01df923aaf7fb5899
philpee2 <philpee2@gmail.com>
2014-07-23 11:58:47 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cuteflix::Application.config.secret_key_base = '70081f1db607bfee0fe53e0fc8925249c007d539e9221136f023330b5ba74dd9c79e0bb818df89b61aae9fbfaadd37318866ca44a3bc8835994d13b84af8159e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cuteflix::Application.config.session_store :cookie_store, key: '_Cuteflix_session'
